4289058f343bc40f75dee04c459fde245106dc82
SAURABH-CHOUDHARI <saurabh.choudhariasus@gmail.com>
2025-01-27 01:08:59 +0530

    Primitives_reference&ENUMS

---
+++ TS_setup/app.js
@@ -0,0 +1,14 @@
+"use strict";
+// // var a =12;
+// // a = 15;
+// // function abcd(){
+// //     let x =12;
+// //     let y=22;
+// // }
+// // let arr:Number[] = [1,2,3,4]
+// // let arr: [String,Number] = ["Hritik",18]
+// enum UserRole{
+//     ADMIN="admin",
+//     GUEST= "guest"
+// }
+// UserRole.ADMIN
